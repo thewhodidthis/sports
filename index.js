1818c40b986412a04e79bd2bdb211f6e33ffdd62
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2016-09-07 10:34:15 +0200

    add comments

--- index.js
+++ index.js
@@ -2,23 +2,44 @@ function Sports() {
   'use strict';
 
   return {
+
+    // IE 9 upwards
     cando: 'addEventListener' in document,
+
+    // IE 11 upwards
     classlist: 'classList' in document.documentElement,
+
+    // Supports file api
+    // TODO: Break apart since recent versions of IE partly support most of these
     fileread: !!(window.File && window.FileReader && window.FileList && window.Blob),
+
+    // Supports fullscreen api
     fullscreen: !!(document.fullscreenEnabled ||
                    document.mozFullScreenEnabled ||
                    document.documentElement.webkitRequestFullScreen),
+
+    // Supports local storage
     localstorage: 'localStorage' in window && window.localStorage !== null,
-    online: navigator.onLine,
+
+    // Is connection avaiable
+    online: window.navigator.onLine,
+
+    // https://www.chromestatus.com/feature/5721832506261504
     scrollsnap: ('scrollSnapType' in document.documentElement.style) ||
                 ('webkitScrollSnapType' in document.documentElement.style),
+
+    // When using home screen launcher
     standalone: 'standalone' in window.navigator && window.navigator.standalone,
-    touch: !!(navigator.userAgent.match(/(iphone|ipad|ipod|android)/gi)),
+
+    // Is touch device
+    touch: !!(window.navigator.userAgent.match(/(iphone|ipad|ipod|android)/gi)),
+
+    // Is webgl available
     webgl: (function () {
       try {
         var test = document.createElement('canvas');
-
-        return !!(window.WebGLRenderingContext && (test.getContext('webgl') || test.getContext('experimental-webgl')));
+        return !!(window.WebGLRenderingContext &&
+                 (test.getContext('webgl') || test.getContext('experimental-webgl')));
       } catch (e) {
         return false;
       }
