1b5f861811e36b40481105c231847d63494f68c3
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-03-17 11:45:58 +0200

    Add test favicon

--- test/index.js
+++ test/index.js
@@ -1,6 +1,14 @@
 const test = require('tape');
 const supports = require('../');
 
+// Add favicon
+const linkTag = document.createElement('link');
+
+linkTag.rel = 'shortcut icon';
+linkTag.href = 'data:;base64,iVBORw0KGgo=';
+
+document.head.appendChild(linkTag);
+
 test('will compute', (t) => {
   // Run all the checks
   Object.keys(supports).forEach((check) => {
