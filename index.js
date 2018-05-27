14251051fef40dd5e435573c289f0eb210eaec67
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2018-05-27 11:23:02 +0100

    module: export functions, because rollup tree shaking

--- index.js
+++ index.js
@@ -3,39 +3,39 @@
 Object.defineProperty(exports, '__esModule', { value: true });
 
 // Server sent events
-const eventsource = 'EventSource' in window;
+const eventsource = () => 'EventSource' in window;
 
 // Device motion API
-const devicemotion = 'DeviceMotionEvent' in window;
-const deviceorientation = 'DeviceOrientationEvent' in window;
+const devicemotion = () => 'DeviceMotionEvent' in window;
+const deviceorientation = () => 'DeviceOrientationEvent' in window;
 
 // File API
 // https://github.com/Modernizr/Modernizr/blob/master/feature-detects/file/api.js
-const filereader = !!(window.File && window.FileList && window.FileReader);
+const filereader = () => !!(window.File && window.FileList && window.FileReader);
 
 // Fullscreen API
 // https://developer.mozilla.org/en/API/Fullscreen
-const fullscreen = !!(document.fullscreenEnabled
-                          || document.mozFullScreenEnabled
-                          || document.documentElement.webkitRequestFullScreen);
+const fullscreen = () => !!(document.fullscreenEnabled
+                                || document.mozFullScreenEnabled
+                                || document.documentElement.webkitRequestFullScreen);
 
 // Connection status
 // https://developer.mozilla.org/en-US/docs/Web/API/NavigatorOnLine/onLine
-const online = 'onLine' in navigator;
+const online = () => 'onLine' in navigator;
 
 // Iframes, app mode and such
 // https://gist.github.com/bennadel/9759788
-const standalone = 'standalone' in navigator && navigator.standalone;
+const standalone = () => 'standalone' in navigator && navigator.standalone;
 
 // Service workers
-const serviceworker = 'serviceWorker' in navigator;
+const serviceworker = () => 'serviceWorker' in navigator;
 
 // Websockets
-const websocket = 'WebSocket' in window;
+const websocket = () => 'WebSocket' in window;
 
 // WebGL rendering
 // https://github.com/mrdoob/three.js/blob/master/examples/js/Detector.js
-const webgl = (() => {
+const webgl = () => {
   try {
     const canvas = document.createElement('canvas');
     const target = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
@@ -44,7 +44,7 @@ const webgl = (() => {
   } catch (x) {
     return false
   }
-})();
+};
 
 exports.eventsource = eventsource;
 exports.devicemotion = devicemotion;
