2e9015888e413ce4b68faae28601605a46643134
Sotiri Bakagiannis <sotirios.bakagiannis@gmail.com>
2021-05-24 01:38:57 +0100

    test: clean up

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 import 'cutaway'
-import { assert, report } from 'tapeless'
-import * as sports from './index.js'
+import { assert, report } from 'tapeless/browser'
+import * as sports from './main.js'
 
 const { ok, notOk } = assert
 
