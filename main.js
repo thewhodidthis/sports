49f45d4910c4a88189fe77912e2ba12a064ce306
Sotiri Bakagiannis <sotirios.bakagiannis@gmail.com>
2021-06-17 22:12:47 +0100

    module: fix for dprint

--- main.js
+++ main.js
@@ -2,11 +2,11 @@
 // Helps detect browser features
 
 // Server sent events
-export const eventsource = () => 'EventSource' in window
+export const eventsource = () => "EventSource" in window
 
 // Device motion API
-export const devicemotion = () => 'DeviceMotionEvent' in window
-export const deviceorientation = () => 'DeviceOrientationEvent' in window
+export const devicemotion = () => "DeviceMotionEvent" in window
+export const deviceorientation = () => "DeviceOrientationEvent" in window
 
 // File API
 // https://github.com/Modernizr/Modernizr/blob/master/feature-detects/file/api.js
@@ -14,33 +14,34 @@ export const filereader = () => !!(window.File && window.FileList && window.File
 
 // Fullscreen API
 // https://developer.mozilla.org/en/API/Fullscreen
-export const fullscreen = () => !!(document.fullscreenEnabled
-                                || document.mozFullScreenEnabled
-                                || document.documentElement.webkitRequestFullScreen)
+export const fullscreen = () =>
+  !!(document.fullscreenEnabled
+    || document.mozFullScreenEnabled
+    || document.documentElement.webkitRequestFullScreen)
 
 // Connection status
 // https://developer.mozilla.org/en-US/docs/Web/API/NavigatorOnLine/onLine
-export const online = () => 'onLine' in navigator
+export const online = () => "onLine" in navigator
 
 // Iframes, app mode and such
 // https://gist.github.com/bennadel/9759788
-export const standalone = () => 'standalone' in navigator && navigator.standalone
+export const standalone = () => "standalone" in navigator && navigator.standalone
 
 // Service workers
-export const serviceworker = () => 'serviceWorker' in navigator
+export const serviceworker = () => "serviceWorker" in navigator
 
 // Websockets
-export const websocket = () => 'WebSocket' in window
+export const websocket = () => "WebSocket" in window
 
 // WebGL rendering
 // https://github.com/mrdoob/three.js/blob/master/examples/js/Detector.js
 export const webgl = () => {
   try {
-    const canvas = document.createElement('canvas')
-    const target = canvas.getContext('webgl') || canvas.getContext('experimental-webgl')
+    const canvas = document.createElement("canvas")
+    const target = canvas.getContext("webgl") || canvas.getContext("experimental-webgl")
 
-    return 'WebGLRenderingContext' in window && target
-  } catch (x) {
+    return "WebGLRenderingContext" in window && target
+  } catch (_) {
     return false
   }
 }
